b7f2bf348aa052b365f203ef8de534814e72f880
Marta Banas <banas.m.marta@gmail.com>
2021-04-09 16:19:51 +0200

    formatting files

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const grid = document.querySelector('.grid');
 
 for (let i = 0; i < 100; i++) {
   const btn = document.createElement('button');
-  btn.className = "btn";
+  btn.className = 'btn';
   grid.appendChild(btn);
 }
