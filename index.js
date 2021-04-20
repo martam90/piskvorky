f7f3b27ac77c0afcf50fc362c21f68a0988eb0bd
Marta Banas <banas.m.marta@gmail.com>
2021-04-20 17:21:34 +0200

    add tic tac toe if/else statement

--- index.js
+++ index.js
@@ -3,7 +3,37 @@
 const grid = document.querySelector('.grid');
 
 for (let i = 0; i < 100; i++) {
-  const btn = document.createElement('button');
+  let btn = document.createElement('button');
   btn.className = 'btn';
   grid.appendChild(btn);
+}
+
+let isPlaying = 'circle';
+
+const buttons = document.querySelectorAll('.btn');
+const playerCircle = document.querySelector('.circle--large');
+const playerCross = document.querySelector('.cross--large');
+
+for (let button of buttons) {
+  button.addEventListener('click', () => {
+    if (isPlaying === 'circle') {
+      let circle = document.createElement('img');
+      circle.src = 'images/circle.svg';
+      circle.className = 'circle--black';
+      button.appendChild(circle);
+      playerCircle.classList.add('active');
+      playerCross.classList.add('active');
+      isPlaying = 'cross'; 
+    } 
+
+    else if (isPlaying === 'cross') {
+      let cross = document.createElement('img');
+      cross.src = 'images/cross.svg';
+      cross.className = 'cross--black';
+      button.appendChild(cross);
+      playerCircle.classList.remove('active');
+      playerCross.classList.remove('active');
+      isPlaying = 'circle'; 
+    }
+  });
 }
