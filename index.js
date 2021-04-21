809165dbd37ceb736f949f8b5010275b8672b375
Marta Banas <banas.m.marta@gmail.com>
2021-04-21 10:16:28 +0200

    modified js code + styles

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
 'use strict';
 
+let isPlaying = 'circle';
 const grid = document.querySelector('.grid');
+const playerCircle = document.querySelector('.circle--large');
+const playerCross = document.querySelector('.cross--large');
 
 for (let i = 0; i < 100; i++) {
   let btn = document.createElement('button');
   btn.className = 'btn';
-  grid.appendChild(btn);
-}
-
-let isPlaying = 'circle';
-
-const buttons = document.querySelectorAll('.btn');
-const playerCircle = document.querySelector('.circle--large');
-const playerCross = document.querySelector('.cross--large');
-
-for (let button of buttons) {
-  button.addEventListener('click', () => {
+  btn.addEventListener('click', () => {
     if (isPlaying === 'circle') {
-      let circle = document.createElement('img');
-      circle.src = 'images/circle.svg';
-      circle.className = 'circle--black';
-      button.appendChild(circle);
-      button.setAttribute('disabled', true);
-      playerCircle.classList.add('active');
+      btn.classList.add('board__field--circle');
+      btn.disabled = true;
+      playerCircle.classList.remove('active');
       playerCross.classList.add('active');
       isPlaying = 'cross'; 
     } else if (isPlaying === 'cross') {
-      let cross = document.createElement('img');
-      cross.src = 'images/cross.svg';
-      cross.className = 'cross--black';
-      button.appendChild(cross);
-      button.setAttribute('disabled', true);
-      playerCircle.classList.remove('active');
+      btn.classList.add('board__field--cross');
+      btn.disabled = true;
+      playerCircle.classList.add('active');
       playerCross.classList.remove('active');
       isPlaying = 'circle'; 
     }
   });
+  grid.appendChild(btn);
 }
